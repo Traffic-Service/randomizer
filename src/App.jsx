f7e2c895a0d177f59adaf72b64d5a823d7e0f800
d33sf0me <katekrokh@gmail.com>
2024-07-31 17:57:11 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,111 @@
+import React, { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  const [min, setMin] = useState(1);
+  const [max, setMax] = useState(100);
+  const [quantity, setQuantity] = useState(10);
+  const [allowDuplicates, setAllowDuplicates] = useState(false);
+  const [sorted, setSorted] = useState(false);
+  const [results, setResults] = useState([]);
+
+  const generateRandomNumbers = () => {
+    let numbers = [];
+    while (numbers.length < quantity) {
+      const num = Math.floor(Math.random() * (max - min + 1)) + min;
+      if (allowDuplicates || !numbers.includes(num)) {
+        numbers.push(num);
+      }
+    }
+    if (sorted) {
+      numbers.sort((a, b) => a - b);
+    }
+    setResults(numbers);
+  };
+
+  return (
+    <React.Fragment>
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Генератор случайных чисел</h1>
+      <div className="card">
+        <h3>Сгенерируйте случайные числа в заданном диапазоне</h3>
+        <div className="input-group">
+          <div className="range">
+            <label>
+              от
+              <input
+                type="number"
+                value={min}
+                onChange={(e) => setMin(Number(e.target.value))}
+              />
+            </label>
+            <label>
+              до
+              <input
+                type="number"
+                value={max}
+                onChange={(e) => setMax(Number(e.target.value))}
+              />
+            </label>
+          </div>
+
+          <label>
+            Количество чисел:
+            <input
+              type="number"
+              value={quantity}
+              onChange={(e) => setQuantity(Number(e.target.value))}
+            />
+          </label>
+          <div className="checkboxes">
+            <div className="checkbox">
+              {' '}
+              <label htmlFor="allowDuplicates">
+                <input
+                  type="checkbox"
+                  id="allowDuplicates"
+                  checked={allowDuplicates}
+                  onChange={(e) => setAllowDuplicates(e.target.checked)}
+                />
+                Повторы
+              </label>
+            </div>
+            <div className="checkbox">
+              <label htmlFor="sorted">
+                <input
+                  type="checkbox"
+                  id="sorted"
+                  checked={sorted}
+                  onChange={(e) => setSorted(e.target.checked)}
+                />
+                Сортировать
+              </label>
+            </div>
+          </div>
+        </div>
+        <button onClick={generateRandomNumbers}>Сгенерировать числа</button>
+      </div>
+
+      {results.length !== 0 && (
+        <div className="card">
+          <div className="results">
+            <h3>Результат:</h3>
+            <h2>{results.join(', ')}</h2>
+          </div>
+          <button>Скопировать</button>
+        </div>
+      )}
+    </React.Fragment>
+  );
+}
+
+export default App;
