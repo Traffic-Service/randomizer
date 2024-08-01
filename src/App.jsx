21316f6730cb97dee3c39f056b5253151f118564
d33sf0me <katekrokh@gmail.com>
2024-08-01 21:05:59 +0300

    logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+
+import viteLogo from '/cube-of-numbers.png';
 import './App.css';
 
 function App() {
@@ -32,12 +32,7 @@ function App() {
   return (
     <React.Fragment>
       <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={viteLogo} className="logo" alt="рандомайзер" />
       </div>
       <h1>Генератор случайных чисел</h1>
       <div className="card">
